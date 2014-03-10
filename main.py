3984e087f72cb0d819987edbd640c8b9b39ce46e
Julian Ortega <jortegac@gmail.com>
2014-03-10 19:43:09 +0100

    Some REST test endpoints added

--- main.py
+++ main.py
@@ -1,27 +1,30 @@
 from flask import Flask, render_template, url_for, request, jsonify
+from SPARQLWrapper import SPARQLWrapper, RDF, JSON
 import requests
 import json
 import urllib2
 
 app = Flask(__name__)
 
-TUTORIAL_REPOSITORY = 'http://localhost:8080/openrdf-sesame/repositories/IWA-AH'
+SPARQL_ENDPOINT = 'http://localhost:8080/openrdf-sesame/repositories/IWA-AH'
 
 @app.route('/')
 def index():
     return render_template('index.html')
     
+# REST Endpoint wrapper for the runQuery function
 @app.route('/sparql', methods=['GET'])
 def sparql():
     app.logger.debug('You arrived at ' + url_for('sparql'))
     app.logger.debug('I received the following arguments' + str(request.args) )
-    
-    endpoint = request.args.get('endpoint', None)
-    query = request.args.get('query', None)
-    
+	
+    query = request.args.get('query', None)    
     return_format = request.args.get('format','JSON')
     
-    
+    return runQuery(query, return_format)
+		
+# Runs the sparql query asking for the results in the specified format
+def runQuery(query, return_format, endpoint=SPARQL_ENDPOINT):
     if endpoint and query :
         sparql = SPARQLWrapper(endpoint)
         
@@ -59,7 +62,29 @@ def sparql():
     else :
         return jsonify({'result': 'Error'})
 
+# Get a list of all the genres
+@app.route('/genres', methods=['GET'])
+def getGenres():
+	sparql = "PREFIX rdfs:<http://www.w3.org/2000/01/rdf-schema#> PREFIX rdf:<http://www.w3.org/1999/02/22-rdf-syntax-ns#> PREFIX ah:<http://purl.org/artsholland/1.0/>SELECT DISTINCT ?genre ?genre_name WHERE { ?genre rdf:type ah:Genre. ?genre rdfs:label ?genre_name} ORDER BY ASC(?genre_name)"
+
+	# Run the query
+   	return runQuery(sparql, 'JSON') 
+	
+# Get a list of all venue types
+@app.route('/venueTypes', methods=['GET'])
+def getVenueTypes():
+	sparql = "PREFIX ah:<http://purl.org/artsholland/1.0/> SELECT DISTINCT ?venue_type ?venue_type_name WHERE {?venue ah:venueType ?venue_type. ?venue_type rdfs:label ?venue_type_name}"
+
+	# Run the query
+   	return runQuery(sparql, 'JSON')
+
+# Get a list of all venues by type
+@app.route('/venuesByType', methods=['GET'])
+def getVenuesByType():
+	type = request.args.get('type', None)
+	sparql = "PREFIX rdfs:<http://www.w3.org/2000/01/rdf-schema#> PREFIX ah:<http://purl.org/artsholland/1.0/> SELECT ?venue ?venue_type ?venue_type_label WHERE {?venue ah:venueType %s }" % (type,)
+	# Run the query
+   	return runQuery(sparql, 'JSON') 	
     
-if __name__ == '__main__':
-    app.debug = False
-    app.run()
+if __name__ == '__main__':    
+    app.run(debug=True)
