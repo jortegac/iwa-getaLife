59fff5bdbb97312f0b050833aa60551ab498983a
Julian Ortega <jortegac@gmail.com>
2014-03-12 18:12:30 +0100

    Some changes to try to make this thing work

--- main.py
+++ main.py
@@ -1,33 +1,29 @@
-from flask import Flask, render_template, url_for, request, jsonify
-from SPARQLWrapper import SPARQLWrapper, RDF, JSON
+from flask import Flask, render_template, url_for, request, jsonify, Response, json
+from SPARQLWrapper import SPARQLWrapper, RDF, JSON, XML, N3
+from StringIO import StringIO
 import requests
 import json
 import urllib2
 
 app = Flask(__name__)
 
+#Local endpoint
 SPARQL_ENDPOINT = 'http://localhost:8080/openrdf-sesame/repositories/IWA-AH'
 
+#ArtsHolland sparql endpoint
+AH_ENDPOINT = 'http://api.artsholland.com/sparql'
+
 PREFIX = """PREFIX dc:<http://purl.org/dc/terms/>
-		PREFIX onto:<http://www.ontotext.com/>
-		PREFIX geo:<http://www.w3.org/2003/01/geo/wgs84_pos#>
-		PREFIX foaf:<http://xmlns.com/foaf/0.1/>
-		PREFIX vcard:<http://www.w3.org/2006/vcard/ns#>
-		PREFIX gn:<http://www.geonames.org/ontology#>
-		PREFIX rdfs:<http://www.w3.org/2000/01/rdf-schema#>
-		PREFIX time:<http://www.w3.org/2006/time#>
-		PREFIX search:<http://rdf.opensahara.com/search#>
-		PREFIX osgeo:<http://rdf.opensahara.com/type/geo/>
-		PREFIX xsd:<http://www.w3.org/2001/XMLSchema#>
-		PREFIX owl:<http://www.w3.org/2002/07/owl#>
-		PREFIX rdf:<http://www.w3.org/1999/02/22-rdf-syntax-ns#>
-		PREFIX sesame:<http://www.openrdf.org/schema/sesame#>
-		PREFIX gr:<http://purl.org/goodrelations/v1#>
-		PREFIX fn:<http://www.w3.org/2005/xpath-functions#>
-		PREFIX ah:<http://purl.org/artsholland/1.0/>
-		PREFIX bd:<http://www.bigdata.com/rdf/search#>"""
-
-
+			PREFIX geo:<http://www.w3.org/2003/01/geo/wgs84_pos#>
+			PREFIX foaf:<http://xmlns.com/foaf/0.1/>
+			PREFIX vcard:<http://www.w3.org/2006/vcard/ns#>
+			PREFIX rdfs:<http://www.w3.org/2000/01/rdf-schema#>
+			PREFIX time:<http://www.w3.org/2006/time#>
+			PREFIX xsd:<http://www.w3.org/2001/XMLSchema#>
+			PREFIX owl:<http://www.w3.org/2002/07/owl#>
+			PREFIX rdf:<http://www.w3.org/1999/02/22-rdf-syntax-ns#>
+			PREFIX ah:<http://purl.org/artsholland/1.0/>"""
+			
 @app.route('/')
 def index():
     return render_template('index.html')
@@ -44,21 +40,20 @@ def sparql():
     return runQuery(query, return_format)
 		
 # Runs the sparql query asking for the results in the specified format
-def runQuery(query, return_format, endpoint=SPARQL_ENDPOINT):
+def runQuery(query, return_format, endpoint=SPARQL_ENDPOINT, jsoni=True):
     if endpoint and query :
         sparql = SPARQLWrapper(endpoint)
         
         sparql.setQuery(query)
         
         if return_format == 'RDF':
-            sparql.setReturnFormat(RDF)
+            sparql.setReturnFormat('RDF')
         else :
             sparql.setReturnFormat('JSON')
             sparql.addCustomParameter('Accept','application/sparql-results+json')
         
-        app.logger.debug('Query:\n{}'.format(query))
-        
-        app.logger.debug('Querying endpoint {}'.format(endpoint))
+        #app.logger.debug('Query:\n{}'.format(query))
+        #app.logger.debug('Querying endpoint {}'.format(endpoint))
         
         try :
             response = sparql.query().convert()
@@ -72,7 +67,10 @@ def runQuery(query, return_format, endpoint=SPARQL_ENDPOINT):
                 return response.serialize(format='turtle')
             else :
                 app.logger.debug('Directly returning JSON format')
-                return jsonify(response)
+                if jsoni == True:
+					return jsonify(response)
+                else:
+					return json.dumps(response)
         except Exception as e:
             app.logger.error('Something went wrong')
             app.logger.error(e)
@@ -111,10 +109,11 @@ def getVenues():
 		filter = filter[:-1]
 		filter = filter + "))"
 		
-	sparql = PREFIX + """SELECT DISTINCT * WHERE {
+	sparql = PREFIX + """SELECT DISTINCT ?venue ?venue_type ?venue_title ?venue_description ?venue_shortDescription ?venue_openingHours ?venue_locationAdress ?venue_latitude ?venue_longitude ?venue_email ?venue_homepage ?venue_geometry WHERE {
 					?venue a ah:Venue .
 					?venue ah:venueType ?venue_type .
 					?venue dc:title ?venue_title .
+					?event ah:venue ?venue.
 					OPTIONAL {?venue dc:description ?venue_description .}
 					OPTIONAL {?venue ah:shortDescription ?venue_shortDescription .}
 					OPTIONAL {?venue ah:openingHours ?venue_openingHours .}
@@ -126,6 +125,68 @@ def getVenues():
 					OPTIONAL {?venue geo:geometry ?venue_geometry .}""" + filter + "}" 
 	# Run the query
    	return runQuery(sparql, 'JSON') 	
+
+@app.route('/events', methods=['GET'])
+def getEvents():
+
+	filter = ""
+	
+	if 'type' in request.args:
+		type = request.args.getlist('type', None)
+		filter = filter + " FILTER(?venue_type IN ("
+		for item in type:
+			filter = filter + item + ","
+		filter = filter[:-1]
+		filter = filter + "))"
+	
+	#Get all the venues
+	sparql = PREFIX + """SELECT DISTINCT ?venue ?venue_type ?venue_title ?venue_description ?venue_shortDescription ?venue_openingHours ?venue_locationAdress ?venue_latitude ?venue_longitude ?venue_email ?venue_homepage ?venue_geometry WHERE {
+					?venue a ah:Venue .
+					?venue ah:venueType ?venue_type .
+					?venue dc:title ?venue_title .
+					?event ah:venue ?venue.
+					OPTIONAL {?venue dc:description ?venue_description .}
+					OPTIONAL {?venue ah:shortDescription ?venue_shortDescription .}
+					OPTIONAL {?venue ah:openingHours ?venue_openingHours .}
+					OPTIONAL {?venue ah:locationAddress ?venue_locationAdress .}			   			   
+					OPTIONAL {?venue geo:lat ?venue_latitude .}
+					OPTIONAL {?venue geo:long ?venue_longitude .}
+					OPTIONAL {?venue vcard:email ?venue_email .}
+					OPTIONAL {?venue foaf:homepage ?venue_homepage .}
+					OPTIONAL {?venue geo:geometry ?venue_geometry .}""" + filter + "}" 
+	# Run the query
+	response = runQuery(sparql, 'JSON', jsoni=False)
+	venues = json.loads(response)
+	toRemove = []
+	for binding in venues["results"]["bindings"]:
+		venueURI = binding["venue"]["value"]
+		events_data = json.loads(getEventsByVenue(venueURI))
+		
+		events = events_data["results"]["bindings"]
+		records = []
+		for event in events:
+			records.append(event["event"]["value"])
+
+		binding["events"] = [dict(event=record) for record in records]	
+			
+	return jsonify(venues)
+	
+def getEventsByVenue(venueURI):
+	sparql = PREFIX + """SELECT DISTINCT * WHERE {
+			?event a ah:Event .
+			?event dc:title ?event_title .
+			OPTIONAL {?event ah:venue ?event_venue .}
+			OPTIONAL {?event ah:eventStatus ?event_status .}
+			OPTIONAL {?event ah:production ?event_production .}
+			OPTIONAL {?event ah:room ?event_room .}
+			OPTIONAL {?event dc:description ?event_description .}
+			OPTIONAL {?event ah:cidn ?event_cidn .}
+			OPTIONAL {?event time:hasBeginning ?event_beg_time}
+			OPTIONAL {?event time:hasEnd ?event_end_time}
+			FILTER(?event_venue = <%s>)
+			}""" % (venueURI,)
+			
+	return runQuery(sparql, 'JSON', jsoni=False)	
     
 if __name__ == '__main__':    
     app.run(debug=True)
